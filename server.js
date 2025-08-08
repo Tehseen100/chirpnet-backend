56bd78d40f974a81e9b450db907690a3f5ad68e7
tehseenjaved100@gmail.com <tehseenjaved100@gmail.com>
2025-08-08 12:57:01 +0500

    Add CORS

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import errorHandler from "./middlewares/error.middleware.js";
 import authRoutes from "./routes/auth.routes.js";
 import chirpRoutes from "./routes/chirp.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-// Load env configuration
+// Load environment variables
 dotenv.config();
 
-// Connect to MongoDB Atlas
-connectDB();
-
+// Initialize express app
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// CORS configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN === "*" ? true : process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+
+// Connect to MongoDB Atlas
+connectDB();
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
